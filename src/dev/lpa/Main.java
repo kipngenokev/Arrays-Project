7ea7a4aed5a38de9d375cf19b9dbc0110fe68776
brian070069 <kkipngeno409@gmail.com>
2025-05-14 18:02:57 +0300

    refined Array commit

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,50 +2,55 @@ package dev.lpa;
 
 import java.util.Arrays;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         int[] myIntArray = new int[10];
+        myIntArray[1] = 45;
+        myIntArray[3] =3;
         myIntArray[5] = 50;
-        System.out.println(Arrays.toString(myIntArray));
 
-        double[] myDoubleArray = new double[10];
-        myDoubleArray[2] = 3;
-        System.out.println(myDoubleArray[2]);
+        System.out.println(myIntArray[5]);
 
-        int[] firstTen = {1,2,3,4,5,6,7,8,9,10};
-        System.out.println("first element :"+firstTen[0]);
+        double[] myDoubleArray = new double[5];
+        myDoubleArray[2] = 3.3;
+        System.out.println(myDoubleArray[0]);
+        String[] twoNames = new String[] {"John","Mark","Kim"};
+
+        int[] firstTen = {7,3,2,4,6,22,34,22,1,88};
+        System.out.println("First :"+firstTen[0]);
         int arrayLength = firstTen.length;
-        System.out.println("Length of the array :"+arrayLength);
-        System.out.println("Last :"+firstTen[arrayLength-1]);
+        System.out.println("Array length = "+arrayLength);
+        System.out.println("Last element :"+firstTen[arrayLength-1]);
 
-        int[] newArray;
-//        newArray = new int[] {5,4,3,2,1};
+        int[] newArray ;
+//        newArray = new int[]{1,2,3,4,5};
         newArray = new int[5];
-        for(int i = 0; i < newArray.length; i++) {
-            newArray[i] = newArray.length - i;
+        for(int i=0;i < newArray.length;i++) {
+            newArray[i] = newArray.length-i;
         }
 
-        for (int i = 0; i < newArray.length; i++) {
+        for(int i = 0;i< newArray.length;i++) {
             System.out.print(newArray[i] + " ");
         }
 
         System.out.println();
 
-        for (int element : newArray) {
-            System.out.println(element);
+        for(int element : newArray) {
+            System.out.print(element + " ");
         }
 
-        Object objectVariable = newArray;
-        if(objectVariable instanceof int[]) {
-            System.out.println("Object variable is really an int array");
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+
+        Object objectArray = newArray;
+        if(objectArray instanceof int[]) {
+            System.out.println("ObjectArray is really an int Array");
         }
 
-        Object[] objectArray = new Object[3];
-        objectArray[0] = "Hello";
-        objectArray[1] = new StringBuilder("World");
-        objectArray[2] = newArray;
-
-        System.out.println(Arrays.toString(objectArray));
+        Object[] newObjArray = new Object[3];
+        newObjArray[0] = "Hello";
+        newObjArray[1] = new StringBuilder("World");
+        newObjArray[2] = newArray;
 
     }
 }
